3c8b39995354957e89c4b39c36041c1c52894329
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2021-09-16 02:00:06 +0500

    Sign Up Functionality Done.

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -10,7 +10,6 @@ class AuthController extends GetxController {
   final loginEmailC = TextEditingController();
   final loginPasswordC = TextEditingController();
   // Register Controllers
-  final signUpNameC = TextEditingController();
   final signUpEmailC = TextEditingController();
   final signUpPasswordC = TextEditingController();
   // Auth State Listener
@@ -18,60 +17,86 @@ class AuthController extends GetxController {
   // Loader Boolean
   RxBool isLoading = false.obs;
 
-  // signup(String email, String password) async {
-  //   try {
-  //     UserCredential userCredential = await auth.createUserWithEmailAndPassword(
-  //         email: email, password: password);
+  signup(BuildContext context, String email, String password) async {
+    if (email == "") {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Email is required"),
+          backgroundColor: Color(0xFFA31103),
+        ),
+      );
+      return;
+    } else if (!GetUtils.isEmail(email)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Email is badly formatted"),
+          backgroundColor: Color(0xFFA31103),
+        ),
+      );
+      return;
+    } else if (password == "") {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Password is required"),
+          backgroundColor: Color(0xFFA31103),
+        ),
+      );
+      return;
+    } else if (password.length < 8) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Password length must be minimum 8 characters"),
+          backgroundColor: Color(0xFFA31103),
+        ),
+      );
+      return;
+    }
+    try {
+      isLoading(true);
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      isLoading(false);
 
-  //     if (userCredential.user != null && userCredential.user!.emailVerified) {
-  //       Get.offAll(HomeScreen());
-  //     } else {
-  //       var response = await Get.defaultDialog(
-  //           title: 'Email Verification',
-  //           titleStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-  //           middleText: 'Your email is not verified.',
-  //           confirm: ElevatedButton(
-  //               onPressed: () {
-  //                 Get.back(result: true);
-  //               },
-  //               child: Text('Click to verify')));
-  //       if (response != null && response == true) {
-  //         await userCredential.user!.sendEmailVerification();
-  //         Get.defaultDialog(
-  //             title: 'Email Verification',
-  //             titleStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-  //             middleText: 'A verification link is sent to $email.',
-  //             confirm: ElevatedButton(
-  //                 onPressed: () {
-  //                   Get.back();
-  //                 },
-  //                 child: Text('Okay')));
-  //       }
-  //     }
-  //   } on FirebaseAuthException catch (e) {
-  //     if (e.code == 'weak-password') {
-  //       Get.defaultDialog(
-  //           title: 'Warning',
-  //           titleStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-  //           middleText: 'Weak password',
-  //           confirm: ElevatedButton(
-  //               onPressed: () {
-  //                 Get.back();
-  //               },
-  //               child: Text('Okay')));
-  //     } else if (e.code == 'email-already-in-use') {
-  //       Get.defaultDialog(
-  //           title: 'Warning',
-  //           titleStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-  //           middleText: 'Email already exists',
-  //           confirm: ElevatedButton(
-  //               onPressed: () {
-  //                 Get.back();
-  //               },
-  //               child: Text('Okay')));
-  //     }
-  //   }
-  // }
+      await Get.defaultDialog(
+          title: 'Success',
+          titleStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          middleText: 'Successfully User Created',
+          confirm: ElevatedButton(
+              style: ElevatedButton.styleFrom(primary: Color(0xFFA31103)),
+              onPressed: () {
+                Get.back();
+              },
+              child: Text('Okay')));
+
+      Get.offAll(HomeScreen());
+    } on FirebaseAuthException catch (e) {
+      isLoading(false);
+
+      if (e.code == 'weak-password') {
+        Get.defaultDialog(
+            title: 'Warning',
+            titleStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            middleText: 'Weak password',
+            confirm: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Color(0xFFA31103)),
+                onPressed: () {
+                  Get.back();
+                },
+                child: Text('Okay')));
+      } else if (e.code == 'email-already-in-use') {
+        Get.defaultDialog(
+            title: 'Warning',
+            titleStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            middleText: 'Email already exists',
+            confirm: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Color(0xFFA31103)),
+                onPressed: () {
+                  Get.back();
+                },
+                child: Text('Okay')));
+      }
+    }
+  }
 
   login(BuildContext context, String email, String password) async {
     if (email == "") {
@@ -101,7 +126,7 @@ class AuthController extends GetxController {
     } else if (password.length < 8) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text("Password length must be 8 characters"),
+          content: Text("Password length must be minimum 8 characters"),
           backgroundColor: Color(0xFFA31103),
         ),
       );
@@ -148,6 +173,7 @@ class AuthController extends GetxController {
             titleStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
             middleText: 'No user found',
             confirm: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Color(0xFFA31103)),
                 onPressed: () {
                   Get.back();
                 },
@@ -158,6 +184,7 @@ class AuthController extends GetxController {
             titleStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
             middleText: 'Username or password is incorrect',
             confirm: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Color(0xFFA31103)),
                 onPressed: () {
                   Get.back();
                 },
